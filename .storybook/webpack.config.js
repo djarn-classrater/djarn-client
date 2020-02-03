bf981bb715ccf515b26a4b4488e1e51c9453b0b4
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-02-04 02:32:42 +0700

    Fix absolute path in storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = ({ config }) => {
   config.resolve.extensions.push('.ts', '.tsx')
   config.resolve.alias = {
     ...config.resolve.alias,
-    '~': path.resolve(__dirname)
+    '~': path.resolve(__dirname, '..')
   }
 
   return config
