3b443dee45660377473229d156a874b21ea2775a
sasirat maneejian <sasirat019@gmail.com>
2020-02-24 19:58:33 +0700

    Add Chart bar

---
+++ stories/Chart.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Chart } from '../components/Chart'
+
+
+export default {
+  components: Chart,
+  title: 'Chart',
+  excludeStories: /.*Data$/,
+}
+
+export const defaultChart = () => {
+  return (
+    <Chart />
+  )
+}
