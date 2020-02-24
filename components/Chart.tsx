47d23a392cc8a622e2d407a0cad76b216b2e0865
sasirat maneejian <sasirat019@gmail.com>
2020-02-24 22:19:39 +0700

    Change margin

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 export const Chart = () => {
   return (
     <div>
-      <div style={{backgroundColor:"#8BC7A4", width: "100%", height: '0.5rem',margin: '3px'}} />
-      <div style={{backgroundColor:"#B7D690", width: "50%", height: '0.5rem',margin: '3px'}} />
-      <div style={{backgroundColor:"#F9D959", width: "30%", height: '0.5rem',margin: '3px'}} />
-      <div style={{backgroundColor:"#F4B451", width: "10%", height: '0.5rem',margin: '3px'}} />
-      <div style={{backgroundColor:"#F09165", width: "20%", height: '0.5rem',margin: '3px'}} />
+      <div style={{backgroundColor:"#8BC7A4", width: "100%", height: '0.5rem',margin: '0.25rem'}} />
+      <div style={{backgroundColor:"#B7D690", width: "50%", height: '0.5rem',margin: '0.25rem'}} />
+      <div style={{backgroundColor:"#F9D959", width: "30%", height: '0.5rem',margin: '0.25rem'}} />
+      <div style={{backgroundColor:"#F4B451", width: "10%", height: '0.5rem',margin: '0.25rem'}} />
+      <div style={{backgroundColor:"#F09165", width: "20%", height: '0.5rem',margin: '0.25rem'}} />
     </div>
   )
 }
