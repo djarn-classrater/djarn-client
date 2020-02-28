a58a4d8fb0ceea9c5a2601f88170b224092b8e8d
sasirat maneejian <sasirat019@gmail.com>
2020-02-28 18:24:51 +0700

    Update layout

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,19 +1,35 @@
 import React from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faChevronCircleRight } from '@fortawesome/free-solid-svg-icons'
 
 export const MassageInput = () => {
   return (
-    <div>
-      <form >
-        <label>
-          <input type="text" placeholder="Write review" style={{borderRadius: "20px" ,borderStyle: "solid", borderWidth: "1px", borderColor: "#F0F0F0", height: "2.25rem",  textIndent: "1rem" , width: "70%"}}/>
-        </label>
-        <button style={{ borderRadius: "20px" ,fontSize: "1rem", width: "2.25rem",height: "2.25rem",margin: "0.625rem" , backgroundColor: "#4B95E9", border: "none", color: "#FFFFFF"}}>
-        <FontAwesomeIcon icon={faChevronRight} />
-      </button>
-      </form>
+    <div style={{ display: 'flex' }}>
+      <div style={{ flex: 1 }}>
+        <input 
+          type="text" 
+          placeholder="Write review" 
+          style={{
+            borderRadius: "20px", 
+            borderStyle: "solid", 
+            borderWidth: "1px", 
+            borderColor: "#F0F0F0", 
+            height: "2.25rem", 
+            textIndent: "1rem" , 
+            width: "100%",
+          }}/>
+      </div>
+      <div style={{ flex: 'none' }}>
+        <FontAwesomeIcon 
+          icon={faChevronCircleRight} 
+          color="#4B95E9"
+          style={{
+            width: '2.25rem',
+            height: '2.25rem',
+          }}
+        />
+      </div>
     </div>
   )
 }
