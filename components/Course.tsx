bf6908f4e27682c470a704bc7d5ee8fe79bebaf3
jnpat <35354784+jnpat@users.noreply.github.com>
2020-02-28 13:05:17 +0700

    change font size

--- components/Course.tsx
+++ components/Course.tsx
@@ -15,14 +15,16 @@ export default({courseName,courseId}: Props) =>{
             display: 'flex'
         }}>
             <div style = {{flex: '1'}}>
-                <h4 style={{margin:0}}>{courseId}</h4>
-                <p style={{margin:0 ,color:'#747070' ,fontSize:'0.9rem'}}>{courseName}</p>
+                <p style={{margin:0,fontSize:'1.5rem'}}>{courseId}</p>
+                <p style={{margin:0 ,color:'#747070' ,fontSize:'1rem'}}>{courseName}</p>
             </div>
             <div style = {{flex: 'none',margin:'auto'}}>
                 <button type = 'button' 
                 style={{
-                borderRadius: '10px',
+                borderRadius: '10rem',
+                margin:'auto',
                 border: '#FFFFFF',
+                fontSize:'1rem',
                 background: '#F2F2F2'}}>Review</button>
             </div>
                 
