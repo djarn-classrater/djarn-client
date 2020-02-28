08a836538b280e94007d6d7b85a158ee03b81612
ekawitjaidee <ekawit_jaidee@cmu.ac.th>
2020-02-28 18:12:22 +0700

    change font and resize magin

--- components/Review.tsx
+++ components/Review.tsx
@@ -16,19 +16,28 @@ export default ({ name, review, date, heart }: Props) => {
       <div style={{
         background: '#FFFFFF 0% 0% no-repeat padding-box',
         padding: '1rem',
-        borderRadius: '2rem',
+        borderRadius: '0.625rem',
         height: 'auto',
-        boxShadow: '1rem 1rem 1rem #0000001A',
+        boxShadow: '0px 10px 25px #0000001A',
         wordBreak: "break-all" //make long word new line
       }}>
         <div style={{ display: 'flex' }}>
           <p style={{ flex: "1", margin: 0, fontSize: '0.8rem' }}>{name}</p>
-          <StarRate/>
+          <StarRate value={3}/>
         </div>
-        <p style={{ margin: 0 }}>{review}</p>
+        <p style={{ margin: '0.25rem 0 0.75rem 0', fontWeight: 400 }}>{review}</p>
         <div style={{ display: 'flex' }}>
-          <p style={{ flex: "1", margin: 0, color: '#747474', fontSize: '0.625rem' }}>{date}</p>
-          <FontAwesomeIcon style={{ flex: 'none', margin: 0, color: heart == true ? 'red' : 'lightgray' }} icon={faHeart} />
+          <p style={{
+            flex: "1", 
+            margin: 0, 
+            color: '#747474', 
+            fontSize: '0.625rem' 
+            }}>{date}</p>
+          <FontAwesomeIcon style={{ 
+            flex: 'none', 
+            margin: 0, 
+            color: heart == true ? '#DE8686' : 'lightgray'
+            }} icon={faHeart} />
         </div>
       </div>
     </>
