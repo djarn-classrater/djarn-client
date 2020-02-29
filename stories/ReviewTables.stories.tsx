9d898ad4ef20702782e87e0b341e4b633f555df2
jnpat <jean_pattiya@hotmail.com>
2020-02-29 18:23:23 +0700

    mix course component

---
+++ stories/ReviewTables.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ReviewTables }  from '../components/ReviewTables'
+import  Course  from '../components/Course'
+
+export default {
+  components: ReviewTables,
+  title: 'ReviewTables',
+  excludeStories: /.*Data$/,
+}
+
+export const defaultReviewTables = () => {
+  return (
+    <>
+    <ReviewTables/>
+
+    </>
+  )
+}
