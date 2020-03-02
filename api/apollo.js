465262e460c8f41901575d7d9a3b34ce54333c11
sasirat maneejian <sasirat019@gmail.com>
2020-03-03 03:46:51 +0700

    Initial apollo provider

---
+++ api/apollo.js
@@ -0,0 +1,13 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import fetch from 'node-fetch'
+import { HttpLink } from 'apollo-link-http'
+
+export const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://34.87.133.81:3000/graphql',
+    credential: 'include',
+    fetch,
+  })
+})
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import App from 'next/app';
+import { ApolloProvider } from '@apollo/react-hooks'
+import { client } from '~/api/apollo'
 import '~/styles/index.css'
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <div className="safe-area-view">
-        <Component {...pageProps} />
-      </div>
+      <ApolloProvider client={client} >
+        <div className="safe-area-view">
+          <Component {...pageProps} />
+        </div>
+      </ApolloProvider>
     );
   }
 }
