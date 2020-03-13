895f6bd947ee4f781d965bf34725472cd80c7b0c
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-03-13 15:10:26 +0700

    Add dotnet extension

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
+/* eslint-disable */
+const withPlugins = require('next-compose-plugins')
 const path = require('path')
+const nextEnv = require('next-env')
+const dotenvLoad = require('dotenv-load')
 
-module.exports = {
+dotenvLoad()
+
+const nextConfig = {
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '~': path.resolve(__dirname)
+      '~': path.resolve(__dirname),
     }
 
     return config
   }
 }
+
+module.exports = withPlugins([
+  nextEnv({}),
+], nextConfig)
