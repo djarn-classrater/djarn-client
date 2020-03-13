ea21aa954c2ca1b64d5d2a324a5760c9a119c360
jnpat <jean_pattiya@hotmail.com>
2020-03-13 19:50:30 +0700

    login page done

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import Link from 'next/link'
 import { UrlObject } from 'url'
 
+import SignInButton from '../components/SignInButton'
+
 const cmuOAuthHref: UrlObject = {
   protocol: 'https',
   hostname: 'oauth.cmu.ac.th',
@@ -19,9 +21,32 @@ const cmuOAuthHref: UrlObject = {
 const Index = () => {
   // return <a href={cmuOauthLink}>Login with CMU account</a>
   return (
-    <Link href={cmuOAuthHref}>
-      <a>Login</a>
-    </Link>
+    <div
+      style={{
+        textAlign: 'center',
+      }}
+    >
+      <p
+        style={{
+          margin: '12rem 5rem 10rem 5rem',
+          fontSize: '2.5rem',
+          fontWeight: 500,
+          color: '#004DC7',
+        }}
+      >
+        DJARN
+      </p>
+      <Link href={cmuOAuthHref}>
+        <SignInButton
+          ButtonName={'Sign in with CMU account'}
+          ButtonColor={'#C1DBFF'}
+          textColor={'#304AAC'}
+          width={'75%'}
+          flex={'none'}
+          size={'1.25rem'}
+        />
+      </Link>
+    </div>
   )
 }
 
