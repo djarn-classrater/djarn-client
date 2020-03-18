39fe650664392e89ecf7a0c14e291fc5ac30e2c2
sasirat maneejian <sasirat019@gmail.com>
2020-03-19 02:05:13 +0700

    Add text loading head

--- components/Rate.tsx
+++ components/Rate.tsx
@@ -11,27 +11,42 @@ type Prop = {
 
 export const Rate: FunctionComponent<Prop> = ({ loading, rate, reviewer }) => {
   return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ textAlign: 'center', flex: '1' }}>
-        <p
-          style={{
-            margin: '1rem',
-            marginBottom: '0',
-            fontSize: '3.25rem',
-            fontStyle: 'normal',
-          }}
-        >
-          {loading ? <Skeleton circle width={'3rem'} height={'3rem'} /> : rate}
-        </p>
-        <p
-          style={{ marginTop: '-1rem', color: '#747474', fontSize: '0.875rem' }}
-        >
-          {loading ? <Skeleton width={'6rem'} /> : reviewer + ' reviewer'}
-        </p>
+    <>
+      {loading ? (
+        <Skeleton width={'9rem'} height={'1.25rem'} />
+      ) : (
+        <h3 style={{ margin: '0' }}>Rate & Review </h3>
+      )}
+      <div style={{ display: 'flex' }}>
+        <div style={{ textAlign: 'center', flex: '1' }}>
+          <p
+            style={{
+              margin: '1rem',
+              marginBottom: '0',
+              fontSize: '3.25rem',
+              fontStyle: 'normal',
+            }}
+          >
+            {loading ? (
+              <Skeleton circle width={'3rem'} height={'3rem'} />
+            ) : (
+              rate
+            )}
+          </p>
+          <p
+            style={{
+              marginTop: '-1rem',
+              color: '#747474',
+              fontSize: '0.875rem',
+            }}
+          >
+            {loading ? <Skeleton width={'6rem'} /> : reviewer + ' reviewer'}
+          </p>
+        </div>
+        <div style={{ flex: '2', marginTop: '2rem' }}>
+          <Chart loading={loading} />
+        </div>
       </div>
-      <div style={{ flex: '2', marginTop: '2rem' }}>
-        <Chart loading={loading} />
-      </div>
-    </div>
+    </>
   )
 }
