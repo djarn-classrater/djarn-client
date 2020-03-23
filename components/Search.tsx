9b34096987631959d6d048e2cf9d376eded5894d
sasirat maneejian <sasirat019@gmail.com>
2020-03-24 00:33:49 +0700

    Add search bar

--- components/Search.tsx
+++ components/Search.tsx
@@ -4,11 +4,37 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export const Search = () => {
   return(
-     <div style={{border: 'solid'}}>
-       <form >
-         <input placeholder="Search" style={{border: 'none'}} />
-         <FontAwesomeIcon icon={faSearch} style={{}} />
-       </form>
-     </div> 
+    <div style={{ display: 'flex',
+      borderRadius: "20px", 
+      borderStyle: "solid", 
+      borderWidth: "1px", 
+      borderColor: "#F0F0F0",
+    }}>
+    <div style={{ flex: 1 }}>
+      <input 
+        type="text" 
+        placeholder="Search" 
+        style={{
+          borderRadius: '20px',
+          height: "2rem", 
+          textIndent: "1rem" , 
+          width: "100%",
+          border: 'none'
+        }}/>
+    </div>
+    <div style={{ flex: 'none' }}>
+      <FontAwesomeIcon 
+        icon={faSearch} 
+        style={{
+          paddingTop: '0.625rem',
+          paddingRight: '1rem',
+          textAlign: 'center',
+          color:"#6C6868",
+          width: '1rem',
+          height: '1rem',
+        }}
+      />
+    </div>
+  </div>
   )
 }
