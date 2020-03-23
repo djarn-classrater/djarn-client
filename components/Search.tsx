a4e9e0e75a622cf2f2c95bd4d257cb66469498f0
sasirat maneejian <sasirat019@gmail.com>
2020-03-24 00:36:36 +0700

    Refactoring code form in search component

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,40 +1,44 @@
-import React , { useEffect, useState, useRef}from 'react'
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export const Search = () => {
-  return(
-    <div style={{ display: 'flex',
-      borderRadius: "20px", 
-      borderStyle: "solid", 
-      borderWidth: "1px", 
-      borderColor: "#F0F0F0",
-    }}>
-    <div style={{ flex: 1 }}>
-      <input 
-        type="text" 
-        placeholder="Search" 
-        style={{
-          borderRadius: '20px',
-          height: "2rem", 
-          textIndent: "1rem" , 
-          width: "100%",
-          border: 'none'
-        }}/>
+  return (
+    <div
+      style={{
+        display: 'flex',
+        borderRadius: '20px',
+        borderStyle: 'solid',
+        borderWidth: '1px',
+        borderColor: '#F0F0F0',
+      }}
+    >
+      <div style={{ flex: 1 }}>
+        <input
+          type="text"
+          placeholder="Search"
+          style={{
+            borderRadius: '20px',
+            height: '2rem',
+            textIndent: '1rem',
+            width: '100%',
+            border: 'none',
+          }}
+        />
+      </div>
+      <div style={{ flex: 'none' }}>
+        <FontAwesomeIcon
+          icon={faSearch}
+          style={{
+            paddingTop: '0.625rem',
+            paddingRight: '1rem',
+            textAlign: 'center',
+            color: '#6C6868',
+            width: '1rem',
+            height: '1rem',
+          }}
+        />
+      </div>
     </div>
-    <div style={{ flex: 'none' }}>
-      <FontAwesomeIcon 
-        icon={faSearch} 
-        style={{
-          paddingTop: '0.625rem',
-          paddingRight: '1rem',
-          textAlign: 'center',
-          color:"#6C6868",
-          width: '1rem',
-          height: '1rem',
-        }}
-      />
-    </div>
-  </div>
   )
 }
