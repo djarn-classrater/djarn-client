4f360cf743219da5ac8e6d6c57eeb6ceba7aa95b
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-03-25 06:50:32 +0700

    Update implemented handle value event

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,21 +1,23 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useRef, useState } from 'react'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const Contrainer = styled.div`
   display: flex;
+  margin-bottom: 1rem;
 `
 
 const Logo = styled.h1`
   flex: none;
   margin: 0 0.25rem;
   color: #114ef5;
+  align-self: center;
 `
 
 const SearchContrainer = styled.div`
   flex: 1;
-  margin: 0.375rem;
+  margin: 0 0.25rem;
   position: relative;
 `
 
@@ -23,19 +25,23 @@ const SearchInput = styled.input`
   border-radius: 1.25rem;
   height: 2rem;
   text-indent: 1rem;
-  width: 100%;
+  float: right;
   border: 2px solid #f5f5f5;
   position: relative;
+  transition: width 0.15s ease-in-out;
+
+  width: ${(props: { active: boolean }) => (props.active ? '100%' : '2.25rem')};
 `
 
-const SearchSpan = styled.span`
+const SearchSpan = styled.a`
   align-items: center;
+  justify-content: center;
   display: inline-flex;
   top: 0;
-  right: 1rem;
+  right: 0;
+  width: 2.375rem;
   height: 100%;
   position: absolute;
-  z-index: 4;
   font-size: inherit;
 `
 
@@ -49,16 +55,33 @@ const Search = forwardRef<
   HTMLInputElement,
   React.InputHTMLAttributes<HTMLInputElement>
 >((props, ref) => {
+  const [active, setActive] = useState<boolean>(false)
+  const inputRef = useRef<HTMLInputElement>()
+  // Bypass ref
+  ref = inputRef
+
   return (
     <Contrainer>
       <Logo>D</Logo>
       <SearchContrainer>
-        {/* <p style={{ display: 'block', position: 'relative', margin: 0 }}> */}
-        <SearchInput {...props} ref={ref} type="text" placeholder="Search" />
-        <SearchSpan>
+        <SearchInput
+          {...props}
+          onBlur={e => {
+            setActive(!!e.target.value)
+          }}
+          ref={inputRef}
+          type="text"
+          width={'2.25rem'}
+          active={active}
+        />
+        <SearchSpan
+          onClick={() => {
+            inputRef.current.focus()
+            setActive(true)
+          }}
+        >
           <Icon icon={faSearch} />
         </SearchSpan>
-        {/* </p> */}
       </SearchContrainer>
     </Contrainer>
   )
