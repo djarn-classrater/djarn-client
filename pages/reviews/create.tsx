6600bb6438cca849954652ad158ec86e08b45935
sasirat maneejian <sasirat019@gmail.com>
2020-03-24 07:42:05 +0700

    Implemented create review graphql

--- pages/reviews/create.tsx
+++ pages/reviews/create.tsx
@@ -1,14 +1,16 @@
 import React, { useRef } from 'react'
 import { gql } from 'apollo-boost'
-
+import Error from 'next/error'
 import WriteReviewArea from '~/components/WriteReviewArea'
 import CourseDetail from '~/components/CourseDetail'
 import CreatReviewButton from '~/components/CreateReviewButton'
 import Cancel from '~/components/Cancel'
 import StarRate from '~/components/StarRate'
 import Layout from '~/components/Layout'
-import { useQuery } from '@apollo/react-hooks'
+import { Query } from 'react-apollo'
+import { useMutation } from '@apollo/react-hooks'
 import { useRouter } from 'next/router'
+import { CourseType, ReviewType } from '~/generated/graphql'
 
 const COURSE = gql`
   query getCourse($courseId: String!) {
@@ -19,9 +21,13 @@ const COURSE = gql`
   }
 `
 const CREATE_REVIEW = gql`
-  mutation createReview($input: CreateReviewInput!) {
-    createReview(input: $input) {
+  mutation createReview($courseId: String!, $context: String, $rate: Int!) {
+    createReview(courseId: $courseId, context: $context, rate: $rate) {
       id
+      studentId
+      courseId
+      context
+      rate
     }
   }
 `
@@ -31,38 +37,52 @@ const WriteReviewPage = () => {
   const textAreaRef = useRef<HTMLTextAreaElement>()
   const starRateRef = useRef<HTMLInputElement>()
 
-  const { data, loading, error } = useQuery(COURSE, {
-    variables: router.query,
-  })
-
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>{error.message}</p>
-
-  const { course } = data
+  /**
+   * Mutation hook for create review.
+   */
+  const [createReview, { loading, error, data }] = useMutation<{
+    review: ReviewType
+  }>(CREATE_REVIEW)
+  /**
+   * Create review action stage.
+   */
+  if (loading) return <p>Sending...</p>
+  if (error) return <p>{JSON.stringify(error)}</p>
+  if (data) return <p>{JSON.stringify(data)}</p>
 
   return (
     <Layout>
-      <CourseDetail {...course} />
+      <Query<{ course: CourseType }> query={COURSE} variables={router.query}>
+        {({ loading, error, data }) => {
+          if (loading) return <CourseDetail loading />
+          if (error) return <Error statusCode={500} />
+
+          const { course } = data
+          return <CourseDetail {...course} />
+        }}
+      </Query>
       <StarRate
         ref={starRateRef}
         styleContainer={{ justifyContent: 'center', marginBottom: '2.5rem' }}
         intialRate={0}
         starSize="2.5rem"
-        onChange={e => console.log(e)}
       />
-      <WriteReviewArea
-        onChange={e => console.log(e)}
-        height={'18.25rem'}
-        ref={textAreaRef}
-      />
+      <WriteReviewArea height={'18.25rem'} ref={textAreaRef} />
       <CreatReviewButton
-        onClick={() =>
+        onClick={() => {
           console.log({
             context: textAreaRef.current.value,
             rate: starRateRef.current.valueAsNumber,
             ...router.query,
           })
-        }
+          createReview({
+            variables: {
+              courseId: router.query.courseId,
+              context: textAreaRef.current.value,
+              rate: starRateRef.current.valueAsNumber,
+            },
+          })
+        }}
       />
       <Cancel />
     </Layout>
