47f26b039e52bbf2c328b0ab25831a083301bf2f
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-03-25 02:43:10 +0700

    Convert jsx to styled-component

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,57 +1,69 @@
 import React, { forwardRef } from 'react'
+import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+const Contrainer = styled.div`
+  display: flex;
+`
+
+const Logo = styled.h1`
+  flex: none;
+  margin: 0 0.25rem;
+  color: #114ef5;
+`
+
+const SearchContrainer = styled.div`
+  flex: 1;
+  margin: 0.375rem;
+  position: relative;
+`
+
+const SearchInput = styled.input`
+  border-radius: 1.25rem;
+  height: 2rem;
+  text-indent: 1rem;
+  width: 100%;
+  border: 2px solid #f5f5f5;
+  position: relative;
+`
+
+const SearchSpan = styled.span`
+  align-items: center;
+  display: inline-flex;
+  top: 0;
+  right: 1rem;
+  height: 100%;
+  position: absolute;
+  z-index: 4;
+  font-size: inherit;
+`
+
+const Icon = styled(FontAwesomeIcon)`
+  display: inline-block;
+  box-sizing: inherit;
+  color: #6c6868;
+`
 
 const Search = forwardRef<
   HTMLInputElement,
   React.InputHTMLAttributes<HTMLInputElement>
 >((props, ref) => {
   return (
-    <div style={{ display: 'flex' }}>
-      <h1 style={{ flex: 'none', margin: 0, color: '#114EF5' }}>D</h1>
-      <div style={{ flex: 1, padding: '0.375rem' }}>
-        <p style={{ display: 'block', position: 'relative', margin: 0 }}>
-          <input
-            {...props}
-            ref={ref}
-            type="text"
-            placeholder="Search"
-            style={{
-              borderRadius: '20px',
-              height: '2rem',
-              textIndent: '1rem',
-              width: '100%',
-              border: '1px solid #E4E4E4',
-              position: 'relative',
-            }}
-          />
-          <span
-            style={{
-              alignItems: 'center',
-              display: 'inline-flex',
-              right: 0,
-              position: 'absolute',
-              zIndex: 4,
-              width: '2rem',
-              top: 0,
-              fontSize: 'inherit',
-              height: '2rem',
-            }}
-          >
-            <FontAwesomeIcon
-              icon={faSearch}
-              style={{
-                display: 'inline-block',
-                boxSizing: 'inherit',
-                color: '#6C6868',
-              }}
-            />
-          </span>
-        </p>
-      </div>
-    </div>
+    <Contrainer>
+      <Logo>D</Logo>
+      <SearchContrainer>
+        {/* <p style={{ display: 'block', position: 'relative', margin: 0 }}> */}
+        <SearchInput {...props} ref={ref} type="text" placeholder="Search" />
+        <SearchSpan>
+          <Icon icon={faSearch} />
+        </SearchSpan>
+        {/* </p> */}
+      </SearchContrainer>
+    </Contrainer>
   )
 })
+
 Search.displayName = 'Search'
 
 export default Search
