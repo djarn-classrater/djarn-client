a9ca3c26ae66f0e875fd6cded8e7f267b4292200
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-03-25 04:38:32 +0700

    Update padding main container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,23 @@
 import App from 'next/app'
 import React from 'react'
+import styled from 'styled-components'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { client } from '~/api/apollo'
 import 'normalize.css'
 import '~/styles/index.css'
+
+const Container = styled.div`
+  padding: 0.5rem;
+`
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
     return (
       <ApolloProvider client={client}>
-        <div className="safe-area-view">
+        <Container className="safe-area-view">
           <Component {...pageProps} />
-        </div>
+        </Container>
       </ApolloProvider>
     )
   }
