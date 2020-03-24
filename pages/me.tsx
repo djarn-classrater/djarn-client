b835276dccc3e8efd807c4b31dedd1046c0c259a
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-03-24 18:56:24 +0700

    Fix user type in graphql

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { gql } from 'apollo-boost'
-import { StudentInfo } from '~/generated/graphql'
+import { UserType } from '~/generated/graphql'
 
 const STUDENT_INFO = gql`
   query getStudentInfo {
@@ -14,7 +14,7 @@ const STUDENT_INFO = gql`
 `
 
 const Me = () => {
-  const { loading, error, data } = useQuery<{ me: StudentInfo }>(STUDENT_INFO)
+  const { loading, error, data } = useQuery<{ me: UserType }>(STUDENT_INFO)
   if (loading) return <p>Loading...</p>
   if (error) return <p>{error.message}</p>
   return <p>{JSON.stringify(data.me)}</p>
